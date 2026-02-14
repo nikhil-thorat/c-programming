6afec2a762667747becca257e29d476d9e907fc6
nikhil-thorat <nikhilthoratofficial@gmail.com>
2026-02-15 01:05:12 +0530

    Basic data type and array

---
+++ src/array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+  // Array of Integers
+  int array[5];
+
+  // Assigning values at each index
+  array[0] = 1;
+  array[1] = 2;
+  array[2] = 3;
+  array[3] = 4;
+  array[4] = 5;
+
+  // Accessing element from the array
+  int second_element = array[1];
+  printf("Second element is %d", second_element);
+
+  return 0;
+}
