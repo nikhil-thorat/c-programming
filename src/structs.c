ad3a5efb786713c6bf7f61836bd0c57acac331ef
nikhil-thorat <nikhilthoratofficial@gmail.com>
2026-02-15 14:36:04 +0530

    Fixed formatting

--- src/structs.c
+++ src/structs.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
 
-struct Person {
-  char *name;
-  int age;
+struct Person
+{
+    char *name;
+    int age;
 };
 
 // typed struct
-typedef struct Point{
-  int x;
-  int y;  
+typedef struct Point
+{
+    int x;
+    int y;
 } Point;
 
-int main() {
+int main()
+{
 
-  struct Person person = {};
-  person.name = "Nikhil";
-  person.age = 22;
+    struct Person person = {};
+    person.name = "Nikhil";
+    person.age = 22;
 
-  printf("Name : %s\n", person.name);
-  printf("Age : %d\n", person.age);
+    printf("Name : %s\n", person.name);
+    printf("Age : %d\n", person.age);
 
-  // Don't need to write struct in the beginning
-  Point point = {6,7};
-  printf("Point X : %d\n", point.x);
-  printf("Point Y: %d\n", point.y);
+    // Don't need to write struct in the beginning
+    Point point = {6, 7};
+    printf("Point X : %d\n", point.x);
+    printf("Point Y: %d\n", point.y);
 
-  return 0;
+    return 0;
 }
